c02101c57a91efc34b84b592c386608c8626d5b7
Guilherme <guilherme@MacBook.local>
2020-06-25 17:25:45 -0300

    refactor/ componentes

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaFacebook, FaInstagram } from 'react-icons/fa';
-import { Footer } from '../../components/footer';
-import { TextAnimated } from '../../components/text-animated';
-import { Schedule } from '../../components/schedule';
+import { Footer } from '../../components/Footer';
+import { TextAnimated } from '../../components/TextAnimated';
+import { Schedule } from '../../components/Schedule';
 
 
 import './styles.css';
@@ -24,27 +24,6 @@ export default function Home() {
                     <img className="banner-image" src={bannerImg} alt="" />
                     <div className="header">
                         <img className="logo" src={logoImg} alt="" />
-                        {/*<nav className="nav navbar navbar-expand-lg navbar-light ">
-                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
-                            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                                <li className="nav-item active">
-                                    <Link className="nav-link" href="#">Inicio<span className="sr-only">(current)</span></Link>
-                                </li>
-                                <li className="nav-item active">
-                                    <Link className="nav-link" href="#">Sobre<span className="sr-only">(current)</span></Link>
-                                </li>
-                                <li className="nav-item active">
-                                    <Link className="nav-link" href="#">Agendamentos<span className="sr-only">(current)</span></Link>
-                                </li>
-                                <li className="nav-item active">
-                                    <Link className="nav-link" href="#">Contatos<span className="sr-only">(current)</span></Link>
-                                </li>
-                                </ul>
-                            </div>
-                        </nav>*/}
                     </div>
                     <div className="texts-container">
                         <div className="row">
